b8c0d8eae95a4e18631a375dec00fa2c26ad64cb
wabisabicode <tretyakov@uni-mainz.de>
2025-02-20 15:29:53 +0100

    add MAIN_PEP_URL to constants.py

--- src/constants.py
+++ src/constants.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
 MAIN_DOC_URL = 'https://docs.python.org/3/'
+MAIN_PEP_URL = 'https://peps.python.org/'
 BASE_DIR = Path(__file__).parent
 DATETIME_FORMAT = '%Y-%m-%d_%H-%M-%S'
