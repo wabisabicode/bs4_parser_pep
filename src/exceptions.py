3bb33423a81f12bf9fe2e8c531f0949c3770b257
wabisabicode <tretyakov@uni-mainz.de>
2025-02-21 16:19:26 +0100

    add VersionNotFoundException

--- src/exceptions.py
+++ src/exceptions.py
@@ -1,2 +1,6 @@
 class ParserFindTagException(Exception):
     """Вызывается, когда парсер не может найти тег."""
+
+
+class VersionNotFoundException(Exception):
+    """Вызывается, когда парсер не может найти ни одной версии."""
